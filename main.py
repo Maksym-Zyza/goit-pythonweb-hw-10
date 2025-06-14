d15d3979e2125fa43a1a89e57b5e21ecf9cf01fc
Maksym.Zyza <mmziza88@gmail.com>
2025-06-14 16:53:36 +0300

    feat: add route with rate limiting

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, Request, status
+from sqlalchemy import text
+
+from slowapi import Limiter
+from slowapi.errors import RateLimitExceeded
+from slowapi.util import get_remote_address
+from starlette.responses import JSONResponse
+
 from src.database.models import Base
 from src.database.db import engine
 from src.database.db import get_db
-from sqlalchemy import text
 from src.routers import contacts, auth
 
 app = FastAPI()
+
+limiter = Limiter(key_func=get_remote_address)
 
 
 @app.on_event("startup")
 async def create_tables():
     Base.metadata.create_all(engine)
+
+
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
+    return JSONResponse(
+        status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+        content={"error": "Rate limit exceeded. Please try again later."},
+    )
 
 
 @app.get("/", name="API root")
@@ -33,6 +49,12 @@ def get_health(db=Depends(get_db)):
             status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
             detail="DB is not configured correctly",
         )
+
+
+@app.get("/me", name="Route with rate limiting")
+@limiter.limit("5/minute")
+async def my_endpoint(request: Request):
+    return {"message": "This is my route with rate limiting."}
 
 
 app.include_router(contacts.router, prefix="/api")
